b32012a1adb6ad7dfa7ea33fd6299686e51e8f99
VietLinh <dtrvietlinh@gmail.com>
2023-05-06 21:27:32 +0700

    add oauth2Login()

--- back-end/src/main/java/api/config/WebSecurityConfig.java
+++ back-end/src/main/java/api/config/WebSecurityConfig.java
@@ -1,20 +1,24 @@
 package api.config;
 
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.authentication.OAuth2LoginAuthenticationToken;
 import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
+@Slf4j
 public class WebSecurityConfig {
 
     public static final String ADMIN = "admin";
@@ -31,12 +35,24 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
                 .requestMatchers("/api/auth/**", "/keycloak/user-provider/**").permitAll()
-                .anyRequest().authenticated()
-                .and();
+                .anyRequest().authenticated();
+        
         http.oauth2ResourceServer()
                 .jwt()
                 .jwtAuthenticationConverter(jwtAuthConverter);
+        
+        http.oauth2Login();      
+        
         http.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
+    }
+    
+    @Bean
+    ApplicationListener<AuthenticationSuccessEvent> successLogger() {
+        return event -> {
+        	OAuth2LoginAuthenticationToken source = (OAuth2LoginAuthenticationToken) event.getAuthentication();
+        	String access_token = source.getAccessToken().getTokenValue();
+            log.info("success: {}", access_token);
+        };
     }
 }
